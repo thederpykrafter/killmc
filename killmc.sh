8d8b1a864c9285324181f3a4d93a8ac02950d517
thederpykrafter <thederpykrafter@gmail.com>
2025-05-04 18:44:54 -0400

    formatting

--- killmc.sh
+++ killmc.sh
@@ -3,10 +3,10 @@
 minecraft="org.prismlauncher.EntryPoint"
 
 if jps -lV | grep $minecraft; then
-  pkill -9 -f $minecraft
-  echo "Killed hanging process"
-  exit 1
+	pkill -9 -f $minecraft
+	echo "Killed hanging process"
+	exit 1
 else
-  echo "Exited successfully"
-  exit 0
+	echo "Exited successfully"
+	exit 0
 fi
